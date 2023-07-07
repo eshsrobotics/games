2de71d8d1a4dbde3618d476780eb60709efa993b
uakotaobi <uche.robotics@akotaobi.org>
2023-07-06 22:13:26 -0700

    Commit the work we did on tile rendering yesterday.
    The code is not complete and does not compile; I intend for us to merge this
    back to main and pick up where we left off the next time we meet.

--- 2023_summer_game/core/src/main/java/com/github/eshsrobotics/watergame/Main.java
+++ 2023_summer_game/core/src/main/java/com/github/eshsrobotics/watergame/Main.java
@@ -1,5 +1,7 @@
 package com.github.eshsrobotics.watergame;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,6 +17,26 @@ public class Main extends ApplicationAdapter {
     private int x;
     private int y;
 
+    // This is the game world.
+    private List<Integer> world_tiles;   
+    private final int world_rows = 5, world_columns = 5;
+    private final int tile_width = 32, tile_height = 32; // In pixels.
+
+    // Our position in the game world, in pixels.
+    private int world_x, world_y; 
+
+    // This is the list of textures that the game world can choose from.
+    private List<Texture> textures;
+
+    private enum TileType {
+        // Only four of these came from OpenGameArt.
+        FLOOR, CORNER_L, CORNER_R, WALL, BLANK
+    }
+
+
+    /**
+     * Code to execute when the game starts.
+     */
     @Override
     public void create() {
         batch = new SpriteBatch();
@@ -22,6 +44,23 @@ public class Main extends ApplicationAdapter {
         sub = new Texture("submarine.png");
         x = 0;
         y = 0;
+
+        // Populate the list of possible textures (so that the world indices
+        // have meaning.)
+        textures = new ArrayList<Texture>(4);
+        textures.put(TileType.CORNER_L, new Texture("Basic_Ground_CornerL_Pixel.png"));
+        textures.put(TileType.CORNER_R, new Texture("Basic_Ground_CornerR_Pixel.png"));
+        textures.put(TileType.WALL, new Texture("Basic_Ground_Filler_Pixel.png"));
+        textures.put(TileType.FLOOR, new Texture("Basic_Ground_Top_Pixel.png"));
+
+        // Make a world.
+        world_tiles = List.of(TileType.WALL,     TileType.BLANK, TileType.WALL,  TileType.WALL,     TileType.BLANK,
+                              TileType.WALL,     TileType.WALL,  TileType.WALL,  TileType.WALL,     TileType.WALL,    
+                              TileType.WALL,     TileType.BLANK, TileType.BLANK, TileType.WALL,     TileType.BLANK,
+                              TileType.CORNER_L, TileType.FLOOR, TileType.FLOOR, TileType.CORNER_R, TileType.BLANK,
+                              TileType.BLANK,    TileType.BLANK, TileType.BLANK, TileType.BLANK,    TileType.BLANK); 
+                            
+                            
     }
 
     @Override
