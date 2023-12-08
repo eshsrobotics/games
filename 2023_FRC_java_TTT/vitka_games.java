67039809cc6b3c433be9a859c3522e305c28b7d1
vithub27 <vescalaboehm_27@esusdstudents.org>
2023-12-07 18:14:25 -0800

    Continued version of the practice tic-tac-toe .
    - Changed box[] from a linear array to a nested one for reasons lost to
      the sands of time.
    - we added a player 2 instead of only having one player. This makes the
      game playable and switches between players.
    - This commit's original date was November 30th.

--- 2023_FRC_java_TTT/vitka_games.java
+++ 2023_FRC_java_TTT/vitka_games.java
@@ -1,24 +1,34 @@
 import java.util.Scanner;
 public class Main {
     public static void main (String args[]) {
+        Boolean gameOver = false;
+        int playerTurn = 1;
         Scanner A = new Scanner(System.in);
-            System.out.print("player 1 choses column");
+        String[][] box = {{"1","2","3"}, {"4","5","6"},{"7","8","9"}};
+        while (!gameOver){
+            System.out.print("player "+playerTurn+" choses column");
             int c = A.nextInt();
-            System.out.print("player 1 choses row");
+            System.out.print("player "+playerTurn+" choses row");
             int r = A.nextInt();
-            String[] box = {" "," "," "," "," "," "," "," "," "};
-            box [                    ]
-        System.out.println("    column 1 column 2 column 3");
-        System.out.println("            |       |      ");
-        System.out.println("row 1   "+box[0]+"   |   "+box[1]+"   |   "+box[2]+"  ");
-        System.out.println("     _______|_______|______");
-        System.out.println("            |       |      ");
-        System.out.println("row 2   "+box[3]+"   |   "+box[4]+"   |   "+box[5]+"  ");
-        System.out.println("     _______|_______|______");
-        System.out.println("            |       |      ");
-        System.out.println("row 3   "+box[6]+"   |   "+box[7]+"   |   "+box[8]+"  ");
-        System.out.println("            |       |      ");
+            if (!box[r-1][c-1].equals("x") && !box[r-1][c-1].equals("o")){
+                if (playerTurn == 1) {
+                    box[r-1][c-1]= "x";
+                    playerTurn = 2;
+                } else {
+                    box[r-1][c-1]= "o";
+                    playerTurn = 1;
+                }
+                System.out.println("    column 1 column 2 column 3");
+                System.out.println("            |       |      ");
+                System.out.println("row 1   "+box[0][0]+"   |   "+box[0][1]+"   |   "+box[0][2]+"  ");
+                System.out.println("     _______|_______|______");
+                System.out.println("            |       |      ");
+                System.out.println("row 2   "+box[1][0]+"   |   "+box[1][1]+"   |   "+box[1][2]+"  ");
+                System.out.println("     _______|_______|______");
+                System.out.println("            |       |      ");
+                System.out.println("row 3   "+box[2][0]+"   |   "+box[2][1]+"   |   "+box[2][2]+"  ");
+                System.out.println("            |       |      ");
+            } else System.out.println("Space has been used, try again");
+        }
     }
 }
-
-
